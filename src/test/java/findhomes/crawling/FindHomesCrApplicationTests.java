2b5cb0ae87adc6d774879fc6dd866227c7d5e298
DingWoonee <slm0523@naver.com>
2024-10-06 19:44:23 +0900

    first commit

---
+++ src/test/java/findhomes/crawling/FindHomesCrApplicationTests.java
@@ -0,0 +1,13 @@
+package findhomes.crawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindHomesCrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
